defcaf1442b52e4c42b0b148f8d7cf8d152cb090
Mattia Bonzi <mattiabonzi@openworks.it>
2019-11-21 15:08:54 +0100

    License dump

--- src/PolyFill/PhpSpreadSheetHtmlStringReaderPolyFill.php
+++ src/PolyFill/PhpSpreadSheetHtmlStringReaderPolyFill.php
@@ -1,11 +1,22 @@
 <?php
 /*
+ * Copyright 2019 Openworks srl
+ *
  * This file is part of the openworks-srl/documentor package.
  *
- * (c) Openworks srl <www.openworks.it>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * A copy of the License is distributed with the software,
+ * if you can't find it, you may obtain a copy of the License at
  *
- * For the full license information, please view the LICENSE
- * file that was distributed with this source code.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  */
 namespace App\PolyFill;
 
